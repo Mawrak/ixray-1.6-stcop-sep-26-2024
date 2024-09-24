d855f8e4b4f744bf3cc5f0f17b6b35e867bec8a4
isoboleskiy <isoboleskiy@gmail.com>
2024-09-24 04:53:23 +0300

    * ADD: New CAgentManagerPlanar without Goap

---
+++ src/xrGame/FRbmkAgentManagerPlanner.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+class CAgentManager;
+class FRbmkAgentManagerPlanner
+{
+public:
+					FRbmkAgentManagerPlanner	(CAgentManager *InOwner);
+					~FRbmkAgentManagerPlanner	();
+	void			Update						();
+private:
+	bool			IsItemSelected				() const;
+	bool			IsEnemySelected				() const;
+	bool			IsDangerSelected			() const;
+	void			RefreshState					(int32 NewState);
+	int32			CurrentState = -1;
+	CAgentManager*	Owner;
+};
