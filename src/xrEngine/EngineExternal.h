2d29bef273474fa11f2c1ca081c4c2e382d69f5f
ForserX <sv3nk@yandex.ru>
2025-01-26 15:19:41 +0300

    Add step wallmark option for `engine_external`
    AIW compatibility

--- src/xrEngine/EngineExternal.h
+++ src/xrEngine/EngineExternal.h
@@ -24,6 +24,7 @@ enum class EEngineExternalGame
 	EnableWeaponInertion,
 	EnableWeaponCollision,
 	EnableArtefactDegradation,
+	EnableActorStepWallmarks,
 	DisableSprintWhileOverweight
 };
 
@@ -51,6 +52,9 @@ public:
 
 	xr_string GetTitle() const;
 	const char* GetPlayerHudOmfAdditional() const;
+	const xr_vector<shared_str> StepWallmarksMaterials() const;
+	const xr_string WallmarkLeft() const;
+	const xr_string WallmarkRight() const;
 
 	bool operator[](const EEngineExternalUI& ID) const;
 	bool operator[](const EEngineExternalPhysical& ID) const;
